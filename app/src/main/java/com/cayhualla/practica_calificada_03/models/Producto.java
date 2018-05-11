19c65d225db50c4272fcc92f03f8528692d2ff69
Linder Hassinger <linderhassinger00@gmail.com>
2018-05-11 14:48:00 -0500

    Initial commit

---
+++ app/src/main/java/com/cayhualla/practica_calificada_03/models/Producto.java
@@ -0,0 +1,92 @@
+package com.cayhualla.practica_calificada_03.models;
+
+/**
+ * Created by Alumno on 11/05/2018.
+ */
+
+public class Producto {
+
+    private Integer id;
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", distrito='" + distrito + '\'' +
+                '}';
+    }
+
+    private String nombre;
+    private String apellido;
+    private String username;
+
+    private String password;
+    private String distrito;
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDistrito() {
+        return distrito;
+    }
+
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
+    }
+
+
+
+    public Producto(Integer id, String nombre, String apellido, String username, String password, String distrito) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.username = username;
+        this.password = password;
+        this.distrito = distrito;
+    }
+
+
+}
